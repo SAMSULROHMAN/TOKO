49f24256a1a0a571cc441885fb569ac4bbfd80ae
SAMSULROHMAN <rohmansamsul91@gmail.com>
2020-04-30 13:24:01 +0700

    beranda

---
+++ resources/views/beranda/component/header.blade.php
@@ -0,0 +1,33 @@
+<header class="site-navbar" role="banner">
+    <div class="site-navbar-top">
+      <div class="container">
+        <div class="row align-items-center">
+
+          <div class="col-6 col-md-4 order-2 order-md-1 site-search-icon text-left">
+          </div>
+
+          <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
+            <div class="site-logo">
+              <a href="index.html" class="js-logo-clone">Distributor Garam</a>
+            </div>
+          </div>
+
+          <div class="col-6 col-md-4 order-3 order-md-3 text-right">
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <nav class="site-navigation text-right text-md-center" role="navigation">
+      <div class="container">
+        <ul class="site-menu js-clone-nav d-none d-md-block">
+          <li class="">
+            <a href="{{ url('/') }}">Home</a>
+          </li>
+          <li><a href="">Produk</a></li>
+          <li><a href="">Tentang Kami</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
