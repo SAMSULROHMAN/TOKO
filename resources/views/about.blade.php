b3103d358fb331b97f937f313b24bc8c85716f17
SAMSULROHMAN <rohmansamsul91@gmail.com>
2020-05-30 12:50:50 +0700

    changes pic

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -58,11 +58,31 @@
             Peruntukannya seperti Garam Konsumsi / Industri / Peternakan / Pengawetan dan lain lain.
             Produk Kami sudah berlisensi SNI, MUI, Dan BPOM
           </p>
-          <p>
-            Kami sebagai Distributor Marketing yang terpercaya dibawah naungan PT.BUDIONO MADURA BANGUN PERSADA. <br>
-            Pamekasan, Madura
-          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 
+{{-- Gambar Ganti Sendiri --}}
+  <div class="site-section border-bottom" data-aos="fade">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-6">
+            <div class="site-section-heading pt-3 mb-4">
+                <h2 class="text-black">CV MUJIARTO SEJAHTERA</h2>
+            </div>
+            <p>
+                Kami sebagai Distributor Marketing yang terpercaya dibawah naungan PT.BUDIONO MADURA BANGUN PERSADA. <br>
+                Pamekasan, Madura
+            </p>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <div class="block-16">
+                <figure>
+                  <img src="{{  asset('img/cv.jpg') }}" alt="Image placeholder" class="img-fluid rounded">
+                </figure>
+              </div>
         </div>
       </div>
     </div>
